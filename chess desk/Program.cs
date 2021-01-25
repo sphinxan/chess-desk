4a0917cdb09b40143436a71ecf7d5ab1d3d76ef9
) <dazaj.osamu@mail.ru>
2021-01-25 22:12:30 +0500

    chess desk

---
+++ chess desk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace chess_desk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteBoard(8);
+            WriteBoard(1);
+            WriteBoard(2);
+            WriteBoard(3);
+            WriteBoard(10);
+        }
+
+        private static void WriteBoard(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                    Console.Write(((i + j) % 2 == 0) ? "#" : ".");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
